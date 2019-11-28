62748ca40dc661ef04ae8f406698aa67fbec9adb
Git-xiaocan <940659468@qq.com>
2019-11-28 12:36:49 +0800

    第二次提交封装数据库操作

--- src/main/java/com/xiaocan/jdbc/util/JdbcUtil.java
+++ src/main/java/com/xiaocan/jdbc/util/JdbcUtil.java
@@ -1,13 +1,31 @@
 package com.xiaocan.jdbc.util;
 
+import sun.jvm.hotspot.tools.SysPropsDumper;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
 
 public class JdbcUtil {
+    private static Properties properties = null;
+    private static final String DatabaseConfig = "dbConfig/DataBaseConfig.properties";
     private static Connection con = null;
     private static Statement stam = null;
+    static {
+        properties = new Properties();
+        InputStream instream = Thread.currentThread().getContextClassLoader().getResourceAsStream(DatabaseConfig);
+        try {
+            properties.load(instream);
+            //System.out.println(properties.getProperty("jdbc.password"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 
     /**
      * 连接数据库
@@ -16,22 +34,16 @@ public class JdbcUtil {
      * @param password
      * @return
      */
-    public static Connection JdbcConnect(String url,String user,String password){
+    public static Connection GetConnection(){
+        String url = properties.getProperty("jdbc.url");
+        String user = properties.getProperty("jdbc.user");
+        String password = properties.getProperty("jdbc.password");
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            con = DriverManager.getConnection(url,user,password);
-            return con;
+            return  DriverManager.getConnection(url,user,password);
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return null;
     }
-
-
 
 }
--- src/test/java/com/xiaocan/jdbc/util/JdbcUtilTest.java
+++ src/test/java/com/xiaocan/jdbc/util/JdbcUtilTest.java
@@ -1,28 +1,43 @@
 package com.xiaocan.jdbc.util;
 
-
 import org.junit.Test;
-import org.junit.runner.RunWith;
+import sun.applet.Main;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import static org.junit.Assert.*;
 
 public class JdbcUtilTest {
 
-
     @Test
-    public void jdbcConnect() {
-        String url = "jdbc:mysql://118.24.220.231:3306/testbase?serverTimezone=UTC&useSSl=false&useUnicode=true";
-       String username = "root";
-       String password = "lzc20017";
+    public void testInsertNew() {
 
-     Connection con = com.xiaocan.jdbc.util.JdbcUtil.JdbcConnect(url,username, password);
-        if (con!=null){
-            System.out.println("连接成功");
-        }else {
-            System.out.println("连接失败");
+        Connection con = JdbcUtil.GetConnection();
+        try {
+            Statement statement = con.createStatement();
+            String insertSql = "insert into student(name,class) values('狗屎','机电2班')";
+              //int row =  statement.executeUpdate(insertSql);
+            ResultSet resultSet = statement.executeQuery("select * from student;");
+           // System.out.println("影响了" + row + "行");
+            while (resultSet.next()){
+
+                System.out.println(resultSet.getInt("Id") + " " + resultSet.getString("name") + " "+resultSet.getString("class"));
+
+            }
+            resultSet.close();
+            statement.close();
+            con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+
         }
 
     }
+
+
+
 }
